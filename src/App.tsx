b54705dddfa302ee68745e7027b20fbdb3ebdcd4
Navjot Singh <62318415+NavjotSingh01@users.noreply.github.com>
2023-01-07 02:21:30 -0500

    assessment updated

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainRoutes from './routes/routes';
+
+function App() {
+  return (
+    <MainRoutes />
+
+  );
+}
+
+export default App;
